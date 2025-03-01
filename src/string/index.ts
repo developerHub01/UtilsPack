dcc026edb94b037288f4c68490ec07d977c57c90
developerHub01 <developerhubytchannel@gmail.com>
2025-03-01 15:09:42 +0600

    docs added for up_toCapitalCase

--- src/string/index.ts
+++ src/string/index.ts
@@ -1,4 +1,12 @@
-String.prototype.up_toCapitalCase = function (full = false): string {
+/**
+ * Capitalizes the first letter of the string.
+ * Optionally, you can capitalize the first letter of each word in the string.
+ *
+ * @param {boolean} [full=false] - If true, capitalizes the first letter of each word in the string.
+ *                                  If false, capitalizes only the first letter of the string.
+ * @returns {string} The transformed string with capitalized letters.
+ */
+String.prototype.up_toCapitalCase = function (full: boolean = false): string {
   if (!full) return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
 
   return this.split(" ")
